db873fe18fbae34dfcf34d29393d4b0516793c65
Gustavo Bicalho <gusbicalho@gmail.com>
2015-06-22 21:27:14 -0300

    Move landing view to states folder to embed in js

--- app/scripts/states/landing.ts
+++ app/scripts/states/landing.ts
@@ -1,5 +1,8 @@
 ﻿///<reference path="../../../typings/project.d.ts" />
 
+import fs = require('fs');
+import stripBom = require('strip-bom');
+
 export function RegisterWith(module: ng.IModule) {
   module.config(configStates);
 }
@@ -8,7 +11,7 @@ function configStates($stateProvider: ng.ui.IStateProvider) {
   $stateProvider
     .state('landing', {
       url: '/',
-      templateUrl: 'views/landing.html',
+      template: stripBom(fs.readFileSync(__dirname+'/landing.html','utf-8')),
       controller: LandingController,
       controllerAs: 'ctrl'
     })
