f7e9a814e1a28f68977e7510f958115cfc527011
Gustavo Bicalho <gusbicalho@gmail.com>
2015-06-24 14:26:31 -0300

    Add link to wp-json to navbar

--- app/index.php
+++ app/index.php
@@ -30,7 +30,7 @@
   		<div id="navbar" class="navbar-collapse" ng-init="navbarCollapsed = true" collapse="navbarCollapsed">
         <!-- These will show up in all pages -->
   			<ul class="nav navbar-nav navbar-left">
-  				<li><a href>Link</a></li>
+  				<li><a href="wp-json">JSON API</a></li>
   				<li class="dropdown" dropdown>
             <a href class="dropdown-toggle" dropdown-toggle>Dropdown</a>
             <ul class="dropdown-menu">
