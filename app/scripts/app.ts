7d31f139e69d5a18c5cba7fa1fa751228b5261fa
Gustavo Bicalho <gusbicalho@gmail.com>
2015-06-25 01:14:21 -0300

    Preload categories and tags

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,10 +1,12 @@
 ﻿///<reference path="../../typings/project.d.ts" />
 
 import _ = require('lodash');
+import WordpressModel = require('./services/wordpress-model');
 
 export function RegisterWith(module: ng.IModule) {
   module.config(config)
         .run(basicSetup)
+        .run(preLoad)
         .controller('AppController',AppController);
 }
 
@@ -25,6 +27,23 @@ function basicSetup($log: ng.ILogService,
     $log.log('$stateChangeError',event,toState,toParams,fromState,fromParams);
     $log.error(error.code, error);
   });
+}
+
+function preLoad(WordpressModel: WordpressModel.Service,
+                 $q: ng.IQService) {
+  $q.all([
+    WordpressModel.categoriesAccessor().get(),
+    WordpressModel.categoriesAccessor().getTerms()
+      .then((terms) => $q.all(
+        _.map(terms, (t) =>
+                      WordpressModel.categoriesAccessor().getTerm(t.slug))
+      )),
+    WordpressModel.tagsAccessor().get(),
+    WordpressModel.tagsAccessor().getTerms()
+      .then((terms) => $q.all(
+        _.map(terms, (t) => WordpressModel.tagsAccessor().getTerm(t.ID))
+      )),
+  ]).then(() => console.log("Preloaded."));
 }
 
 class AppController {
